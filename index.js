2716a5a349d76c72660a00a71f690bc680211e4e
Daniela Leal <falfada@gmail.com>
2024-05-09 13:35:41 +1000

    Added Inquirer and prompt functionality

---
+++ index.js
@@ -0,0 +1,40 @@
+// Prompts text (up to three characters)
+// Prompt for text color
+// Prompt for shape in a list(circle, triangle, and square)
+// Prompt for shape color
+// Create a file with the name "logo.svg" 300x200px svg image
+// Output text `Generated logo.svg`
+const inquirer = require('inquirer');
+const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt');
+
+inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt);
+
+function handleResponse(answers){
+    console.log(answers);
+}
+const questions = [
+    {
+        type: 'maxlength-input',
+        name: 'logoText',
+        message: 'Enter your logo text',
+        maxLength: 3
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'Enter the text color keyword or hexadecimal number'
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'Choose a shape',
+        choices: ['Circle', 'Triangle', 'Square']
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter the shape color keyword or hexadecimal number'
+    }
+];
+
+inquirer.prompt(questions).then(handleResponse);
